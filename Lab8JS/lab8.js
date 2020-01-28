d779deea8262b969a9135bba686dd36b45afa066
BluEyesWhiteJason <jason.speet@gmail.com>
2020-01-27 22:40:51 -0500

    started lab 8

---
+++ Lab8JS/lab8.js
@@ -0,0 +1,11 @@
+class Student{
+    constructor(name, hometown, food){
+        this.Name = name;
+        this.Hometown = hometown;
+        this.Food = food;
+    }
+}
+
+s1 = new Student("Jerry", "Holland", "Ants");
+s2 = new Student("Anna", "Midland", "Pasghetti");
+
